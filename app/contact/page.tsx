53c86899c4cb190a61a79d05dc336875b74c971d
Rajkumar Chaudhari <rajkumr.gie@gmail.com>
2026-05-29 12:21:47 +0530

    Update all contact phone numbers in navbar, footer, and contact page to 8800759535

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ import { MdOutlineSupportAgent } from "react-icons/md";
 import { BsSendCheck } from "react-icons/bs";
 
 export default function ContactPage() {
-  const phoneNumber = "+919667048566";
+  const phoneNumber = "+918800759535";
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
 
